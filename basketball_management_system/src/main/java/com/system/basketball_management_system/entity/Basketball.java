15268809a805e26128828e7973cebe4df5dc8140
220049 <220049@softwarica.edu.np>
2023-07-18 11:28:05 +0545

    entity

---
+++ basketball_management_system/src/main/java/com/system/basketball_management_system/entity/Basketball.java
@@ -0,0 +1,46 @@
+package com.system.basketball_management_system.entity;
+
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Basketball")
+
+public class Basketball {
+    @Id
+    @SequenceGenerator(name = "shb_product_seq_gen", sequenceName = "shb_product_id_seq", allocationSize = 1)
+    @GeneratedValue(generator = "shb_product_seq_gen", strategy = GenerationType.SEQUENCE)
+    private Integer basket_ballId;
+
+    private String basketballname;
+
+    private String basketballprice;
+
+    private String basketballcontact;
+
+    private String basketballlocation;
+
+    private String basketballimage;
+    private String basketballimage1;
+    private String basketballimage2;
+
+    private String Description;
+
+    @Transient
+    private String imageBase64;
+
+    @Transient
+    private String image1Base64;
+
+
+    @Transient
+    private String image2Base64;
+
+}
